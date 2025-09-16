d91a664dcd4a9638b7932cbc3ae18486e61aa1d3
Aurel-Charles <charlesaurelienpro@gmail.com>
2025-09-16 10:59:08 +0200

    Add a loop to trigger mouse hover
    when the mouse hover a square, it change his classe to square-colored
    which is style with a different background color

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
-console.log('hello');
-
+console.log('Let\'s get some colore');
+///////////////////////
 const container = document.querySelector('#container')
 const div = document.createElement('div')
 
-
-
 function createGridOfDiv() {
-    for (let i = 0; i <= 255; i++) {
+    for (let i = 1; i <= 256; i++) {
         const div = document.createElement('div')
         div.className = 'square'
+        div.classList.add('square-uncolored')
         div.id = `square-${i}`
         container.appendChild(div)       
     }
  
 }
 
- 
 createGridOfDiv()
+
+//// hover effect
+
+
+
+const squares = document.querySelectorAll('.square')
+for (const square of squares) {
+    square.addEventListener('mouseenter', ()=> {
+        square.className = 'square-colored'
+        console.log('hovered');
+    })
+
+}
+
