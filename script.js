b044bd7b9ecfbbe02b65887871525dfe90201509
Aurel-Charles <charlesaurelienpro@gmail.com>
2025-09-16 09:45:42 +0200

    Add function to create square <div>

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log('hello');
+
+const container = document.querySelector('#container')
+const div = document.createElement('div')
+
+
+
+function createGridOfDiv() {
+    for (let i = 0; i <= 16; i++) {
+        const div = document.createElement('div')
+        div.className = 'square'
+        div.id = `square-${i}`
+        container.appendChild(div)       
+    }
+ 
+}
+
+ 
+createGridOfDiv()
